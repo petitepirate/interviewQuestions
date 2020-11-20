f0fe161b187167ea959e42a2fcce18ddffe60074
megan <msmeganmcmanus@gmail.com>
2020-11-19 19:12:38 -0800

    add question 85

---
+++ q0085.py
@@ -0,0 +1,17 @@
+# This problem was asked by Facebook.
+
+# Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, 
+# using only mathematical or bit operations. You can assume b can only be 1 or 0.
+
+#_____________________________________________________________________________
+# Solution
+# We can solve this problem by seeing that if we multiply x with b, it solves 
+# half the problem. Since we want y to behave in opposite, we can get the same 
+# behavior by multiplying y with (1 - b).
+
+# Now, (x * b) gives x when b is 1 and 0 otherwise. Similarly, (y * (1 - b)) 
+# gives y when b is 0 and 0 otherwise. We can just combine the two formulas with 
+# either a + or |,
+
+def switch(x, y, b):
+  return (x * b) | (y * (1 - b))
